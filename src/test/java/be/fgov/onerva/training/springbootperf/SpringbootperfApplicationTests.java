dbc4bac41cc6d83f1d49f65dc85213b27ac9b36f
krigot <kevin.rigot@onem.be>
2025-11-13 14:36:36 +0100

    Unit test in memory db

--- src/test/java/be/fgov/onerva/training/springbootperf/SpringbootperfApplicationTests.java
+++ src/test/java/be/fgov/onerva/training/springbootperf/SpringbootperfApplicationTests.java
@@ -2,8 +2,11 @@ package be.fgov.onerva.training.springbootperf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringbootperfApplicationTests {
 
 	@Test
